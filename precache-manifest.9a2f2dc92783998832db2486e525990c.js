15a848af5795f1ac554f9d9f8a3b39716c249e7e
Jan Kozieł <48291123+janKozie1@users.noreply.github.com>
2020-04-16 19:35:47 +0200

    Updates

---
+++ precache-manifest.9a2f2dc92783998832db2486e525990c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "794081224895c9a6f29e10ef512b60cc",
+    "url": "/pokemon-lookup/index.html"
+  },
+  {
+    "revision": "5fb89ac1df619986883e",
+    "url": "/pokemon-lookup/static/js/2.2684d34d.chunk.js"
+  },
+  {
+    "revision": "68974a4be31a34d941f3",
+    "url": "/pokemon-lookup/static/js/main.11ec90a9.chunk.js"
+  },
+  {
+    "revision": "5593bde73c940ab38735",
+    "url": "/pokemon-lookup/static/js/runtime~main.ed3e6c9a.js"
+  }
+]);
